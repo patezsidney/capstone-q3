5112500950947cffbe327336c81eb27ce485aca6
Felipe Silveira <felipelarson@msn.com>
2022-03-02 16:46:55 +0000

    feat(library_model): added dataclass

--- app/models/library_model.py
+++ app/models/library_model.py
@@ -1,14 +1,15 @@
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from sqlalchemy import Column, String, Date
 from app.configs.database import db
 
 
-
+@dataclass
 class LibraryModel(db.Model):
 
     __tablename__ = "library"
 
-    library_id = Column(String, primary_key=True)
-    date_withdrawal = Column(Date, nullable=False, default=datetime.now())
-    date_return = Column(Date, nullable=True)
-    date_accurancy = Column(Date, nullable=False, default=(datetime.now() + timedelta(15)))
+    library_id: str = Column(String, primary_key=True)
+    date_withdrawal: str = Column(Date, nullable=False, default=datetime.now())
+    date_return: str = Column(Date, nullable=True)
+    date_accurancy: str = Column(Date, nullable=False, default=(datetime.now() + timedelta(15)))
