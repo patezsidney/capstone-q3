594848639880d8ee9b7f1df0e9b58866e7d5bc84
Felipe Silveira <felipelarson@msn.com>
2022-03-02 16:45:22 +0000

    feat(books_model): added dataclass

--- app/models/books_model.py
+++ app/models/books_model.py
@@ -1,13 +1,14 @@
+from dataclasses import dataclass
 from sqlalchemy import Column, String, Integer
 from app.configs.database import db
 
-
+@dataclass
 class BooksModel(db.Model):
 
     __tablename__ = "books"
 
-    book_id = Column(String, primary_key=True)
-    title = Column(String(255), nullable=False)
-    author = Column(String(255), nullable=False)
-    quantity = Column(Integer)
+    book_id: str = Column(String, primary_key=True)
+    title: str = Column(String(255), nullable=False)
+    author: str = Column(String(255), nullable=False)
+    quantity: int = Column(Integer)
 
