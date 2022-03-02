e256dbd81bdc43d1dfb3c8ebb70e8c3f72b0807b
Felipe Silveira <felipelarson@msn.com>
2022-03-02 17:15:41 +0000

    feat(books_models): added uuid4

--- app/models/books_model.py
+++ app/models/books_model.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 from sqlalchemy import Column, String, Integer
 from app.configs.database import db
 
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 @dataclass
 class BooksModel(db.Model):
 
     __tablename__ = "books"
 
-    book_id: str = Column(String, primary_key=True)
+    book_id: str = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     title: str = Column(String(255), nullable=False)
     author: str = Column(String(255), nullable=False)
     quantity: int = Column(Integer)
