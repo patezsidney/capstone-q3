f033b91de3af58afc4f4c26d646e26205fdd95e8
Sidney Patez <patezsidney.jr@gmail.com>
2022-03-02 16:10:17 -0300

    feat: mergin migrations heads

---
+++ migrations/versions/f2c30558453a_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: f2c30558453a
+Revises: ff4ebc3ff425, 13f9b319b116
+Create Date: 2022-03-02 16:09:32.824421
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f2c30558453a'
+down_revision = ('ff4ebc3ff425', '13f9b319b116')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
