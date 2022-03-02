a845e0106afada220cd05262fe8ec0799daa468a
Felipe Silveira <felipelarson@msn.com>
2022-03-02 17:14:03 +0000

    feat(library_model): added uuid4

--- app/models/library_model.py
+++ app/models/library_model.py
@@ -3,13 +3,15 @@ from datetime import datetime, timedelta
 from sqlalchemy import Column, String, Date
 from app.configs.database import db
 
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 
 @dataclass
 class LibraryModel(db.Model):
 
     __tablename__ = "library"
 
-    library_id: str = Column(String, primary_key=True)
+    library_id: str = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     date_withdrawal: str = Column(Date, nullable=False, default=datetime.now())
     date_return: str = Column(Date, nullable=True)
     date_accurancy: str = Column(Date, nullable=False, default=(datetime.now() + timedelta(15)))
