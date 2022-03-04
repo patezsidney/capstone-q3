2d172c57523ced39a3294e170fe727efccdf1bf3
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-03-04 12:52:18 -0300

    refactor(app.controller): controller cleaning

--- app/controllers/classroom_controllers.py
+++ app/controllers/classroom_controllers.py
@@ -1,16 +1,5 @@
-from app.configs.database import db
-from app.models.classroom_model import ClassroomModel
-from flask import current_app, request , jsonify
-
 def create_classroom():
-    data = request.get_json()
-
-    classroom = ClassroomModel(**data)
-
-    current_app.db.session.add(classroom)
-    current_app.db.session.commit()
-
-    return ""
+    pass
 
 def update_classroom(id: str):
     pass
--- app/controllers/students_controllers.py
+++ app/controllers/students_controllers.py
@@ -1,18 +1,5 @@
-from http import HTTPStatus
-from app.models.students_model import StudentsModel
-from flask import request, current_app, jsonify
-from secrets import token_urlsafe
-
 def sigin():
-    data = request.get_json()
-    data["api_key"] = token_urlsafe(16)
-
-    student = StudentsModel(**data)
-
-    current_app.db.session.add(student)
-    current_app.db.session.commit()
-
-    return jsonify(student),HTTPStatus.CREATED
+    pass
 
 def create_student():
     pass
