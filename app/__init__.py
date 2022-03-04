f4e44e1f918bec6906081eeb86081577797e5a39
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2022-03-04 15:59:54 -0300

    feat: end of file populate database

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from app import routes
 from app.configs import database, migrations
+from tests.database_populate import populate_database
 
 
 def create_app():
@@ -11,5 +12,6 @@ def create_app():
     database.init_app(app)
     migrations.init_app(app)
     routes.init_app(app)
+#    populate_database(app)
 
     return app
