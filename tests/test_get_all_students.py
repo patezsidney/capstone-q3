c70046e527e5becd493f253ea93ea591487584bf
Felipe Silveira <felipelarson@msn.com>
2022-03-04 17:14:13 +0000

    feat(test_get_all_students): maked test get all

--- tests/test_get_all_students.py
+++ tests/test_get_all_students.py
@@ -1,9 +1,10 @@
-def test_login_students(client):
+def test_get_all_students(client):
     response = client.get("/students")
-    assert response.get_json()[''] == ''
+
+    assert (type(response.get_json()) is dict), "Verificar se está retornando um dict"
 
 
 def test_status_rota_get_all_stundets(client):
     response = client.get("/students")
 
-    assert response.status_code == 200, "Status Incorreto"
+    assert response.status_code == 200, "Verificar se o status code é created"
