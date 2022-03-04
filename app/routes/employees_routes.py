e170af1d13787f48297d887c1687c7244e79f903
Felipe Silveira <felipelarson@msn.com>
2022-03-04 17:49:14 +0000

    feat(get_all_employees): created function

--- app/routes/employees_routes.py
+++ app/routes/employees_routes.py
@@ -5,3 +5,4 @@ from app.controllers import employees_controllers
 bp = Blueprint("employees", __name__, url_prefix="/employees")
 
 bp.post("")(employees_controllers.create_employee)
+bp.get("")(employees_controllers.get_all_employees)
