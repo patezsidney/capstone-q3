517907131a74432b15e2409e9529754842fe48d9
Felipe Silveira <felipelarson@msn.com>
2022-03-04 13:28:07 +0000

    feat(signin): created function sign in

--- app/controllers/students_controllers.py
+++ app/controllers/students_controllers.py
@@ -1,5 +1,21 @@
-def sigin():
-    pass
+from http import HTTPStatus
+from flask import request
+from app.models.students_model import StudentsModel
+
+
+def signin():
+    user_data = request.get_json()
+    found_user: StudentsModel = StudentsModel.query.filter_by(cpf=user_data["cpf"]).first()
+
+    if not found_user:
+        return {"message": "User not found"}, HTTPStatus.NOT_FOUND
+
+    if found_user.verify_password(user_data["password"]):
+        return {"api_key": found_user.api_key}, HTTPStatus.OK
+
+    else:
+        return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+
 
 def create_student():
     pass
