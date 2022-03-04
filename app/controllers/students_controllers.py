7c87cf7c487bed4ff0a7f557fc2d7a8f969943c1
Felipe Silveira <felipelarson@msn.com>
2022-03-04 14:03:31 +0000

    feat(get_all_students): added auth

--- app/controllers/students_controllers.py
+++ app/controllers/students_controllers.py
@@ -1,6 +1,10 @@
 from http import HTTPStatus
 from flask import current_app, jsonify, request
 from app.models.students_model import StudentsModel
+from flask_httpauth import HTTPTokenAuth
+
+
+auth = HTTPTokenAuth(scheme="Bearer")
 
 
 def signin():
@@ -26,6 +30,8 @@ def update_student():
 def delete_student():
     pass
 
+
+@auth.login_required
 def get_all_students():
     data = current_app.db.session.query(StudentsModel).all()
 
