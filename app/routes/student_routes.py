f25fcd2ae4477705a467e0900f78a3781a3a726a
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-03-04 18:36:51 -0300

    refactor(app):git pull

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
 
-from app.controllers import students_controllers
-from app.controllers import classroom_controllers
+from app.controllers import example_controllers
 
-bp = Blueprint('student_api', __name__, url_prefix='/student')
 
-bp.post("/sign")(students_controllers.sigin)
-bp.post("/classroom")(classroom_controllers.create_classroom)
+bp = Blueprint('student_api', __name__, url_prefix='/api')
+
+bp.get("/sign")(example_controllers.get_example_controller)
