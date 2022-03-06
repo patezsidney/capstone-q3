a7f7d02634777e2640545107eba19163b6cfe261
Renato Suguiy <renatosuguiy@gmail.com>
2022-03-06 00:12:15 -0300

    feat(employee_get_one): Create test for the route

--- tests/test_employees.py
+++ tests/test_employees.py
@@ -1,4 +1,5 @@
 from flask.testing import FlaskClient
+from app.models.employee_model import EmployeeModel
 
 def test_create_employee(client: FlaskClient):
     request_data = {
@@ -26,3 +27,20 @@ def test_get_all_employees(client):
     request_response = client.get("/api/employees")
 
     assert (request_response.status_code == 200), "Verificar se o status code é ok"
+
+def test_get_employee_error(client: FlaskClient):
+    request_data = {
+        "name": "Jhon Doe2",
+        "email": "jhondoe12@mail.com",
+        "wage": 3020.90,
+        "access_level": "admin",
+        "password": "1234",
+        "api_key": "7VyrFqAJeDa-EOBK457GsQ"
+    }    
+    request_response = client.post("/api/employees", json=request_data, follow_redirects=True )
+    response_json: dict = request_response.get_json()
+
+    request_response_get = client.get(f"/api/employees/{response_json['employee_id']}", json=request_data, follow_redirects=True)
+    response_get_json: dict = request_response_get.get_json()
+
+    assert (request_response_get.status_code == 401), "Verificar se o status code é 401"
