b191ec006bc14174496beb5549b99055d435753f
Renato Suguiy <renatosuguiy@gmail.com>
2022-03-06 18:02:35 -0300

    feat(classroom_patch): Created function to edit a classroom

--- app/controllers/classroom_controllers.py
+++ app/controllers/classroom_controllers.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 from flask import jsonify, request
 from sqlalchemy.orm.session import Session
+from sqlalchemy import exc
 
 from app.configs.database import db
 from app.models.classroom_model import ClassroomModel
@@ -17,8 +18,22 @@ def create_classroom():
 
     return jsonify(classroom), HTTPStatus.CREATED
 
-def update_classroom(id: str):
-    pass
+def update_classroom(classroom_id: str):
+    try:
+        data = request.get_json()
+        classroom: ClassroomModel = ClassroomModel.query.get(classroom_id)
+        if classroom is None: 
+            return {'msg': 'Classroom not found'}, HTTPStatus.NOT_FOUND
+
+        for key, value in data.items():    
+            setattr(classroom, key, value)    
+        
+        db.session.add(classroom)
+        db.session.commit()
+
+        return jsonify(classroom), HTTPStatus.OK
+    except exc.IdentifierError:
+        return {'msg': 'Classrom id already exists'}, HTTPStatus.CONFLICT
 
 def delete_classroom(classroom_id: str):
     classroom: ClassroomModel = ClassroomModel.query.get(classroom_id)
