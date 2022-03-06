50cd00dd8d0c1897d28e0e73e99487a4c3e1b133
Renato Suguiy <renatosuguiy@gmail.com>
2022-03-06 00:54:30 -0300

    feat(employee_patch): Created patch  route for employee

--- app/controllers/employees_controllers.py
+++ app/controllers/employees_controllers.py
@@ -1,11 +1,13 @@
 from flask import request, jsonify
 from http import HTTPStatus
 from secrets import token_urlsafe
+from sqlalchemy import exc
 from sqlalchemy.orm.session import Session
 
 from app.configs.database import db
 
 from app.models.employee_model import EmployeeModel
+from app.services.decorators import verify_some_keys
 
 def sigin():
     pass
@@ -23,8 +25,32 @@ def create_employee():
 
     return jsonify(employee), HTTPStatus.CREATED
 
-def update_employee():
-    pass
+@verify_some_keys(['name', 'email', 'wage', 'access_level','password' ,'school_subjects'])
+def update_employee(employee_id: str):
+    try: 
+        data = request.get_json()
+
+        employee: EmployeeModel = EmployeeModel.query.get(employee_id)
+        if employee is None: 
+            return {'msg': 'Employee not found'}, HTTPStatus.NOT_FOUND
+
+        password = data.get('password')
+
+        if password is not None:
+            password = data.pop('password')
+
+        for key, value in data.items():    
+            setattr(employee, key, value)    
+        
+        if password is not None:
+            employee.password = password
+
+        db.session.add(employee)
+        db.session.commit()
+
+        return jsonify(employee), HTTPStatus.OK
+    except exc.IntegrityError:
+        return {'msg': 'Email already exists'}, HTTPStatus.CONFLICT
 
 def delete_employee():
     pass
--- app/routes/employees_routes.py
+++ app/routes/employees_routes.py
@@ -6,3 +6,4 @@ bp = Blueprint("employees", __name__, url_prefix="/employees")
 
 bp.post("")(employees_controllers.create_employee)
 bp.get("")(employees_controllers.get_all_employees)
+bp.patch("/<employee_id>")(employees_controllers.update_employee)
