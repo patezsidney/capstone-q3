5f8a6d1f73d2682c073170df974bf686b0a57d83
Renato Suguiy <renatosuguiy@gmail.com>
2022-03-06 17:50:21 -0300

    feat(classroom_test): Created test to delete a classroom

--- tests/test_clasrooms.py
+++ tests/test_clasrooms.py
@@ -20,3 +20,21 @@ def test_create_classroom(client: FlaskClient):
     assert (response_keys == expected_keys), "verifique as keys retornadas"
     assert (type(response_json) is dict), "Verificar se está retornando um dict"
     assert (request_response.status_code == 201), "Verificar se o status code é created"
+
+def test_delete_success(client):
+    request_data = {
+        "name": "2B"
+    }
+
+    request_response = client.post("/api/classrooms", json=request_data, follow_redirects=True )
+    response_json: dict = request_response.get_json()
+
+    request_delete = client.delete(f"/api/classrooms/{response_json['classroom_id']}")
+
+    assert (request_delete.status_code == 204), "Verificar se o status code é No Content"
+
+def test_delete_error(client):
+
+    request_delete = client.delete(f"/api/classrooms/b3298cfc-7fb8-47af-91ed-f2d8c4545cdd")
+
+    assert (request_delete.status_code == 404), "Verificar se o status code é Not Found"
