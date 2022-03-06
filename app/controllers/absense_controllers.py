7435c0cd17d5923d23192803734b0825209b1fa8
Arthur Sarmento <arthurbs17@gmail.com>
2022-03-06 15:38:17 -0300

    feat(get_absences_by_student_id): created function to get absences by student id

--- app/controllers/absense_controllers.py
+++ app/controllers/absense_controllers.py
@@ -1,3 +1,11 @@
+from flask import jsonify
+from http import HTTPStatus
+
+from sqlalchemy.exc import DataError
+
+from app.models.students_model import StudentsModel
+
+
 def create_absense():
     pass
 
@@ -10,6 +18,20 @@ def delete_absense(id: str):
 def get_all_absense():
     pass
 
-def get_student_absense(id: str):
-    pass
+def get_student_absense(student_id: str):
+    
+    try:
+        student: StudentsModel = StudentsModel.query.filter_by(
+        registration_student_id = student_id
+    ).first()
+
+        response = {
+            "name": student.name,
+            "absences": student.absences
+        }
+
+        return jsonify(response), HTTPStatus.OK
+    
+    except DataError:
+        return {"msg": "student not found!"}, HTTPStatus.NOT_FOUND
 
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, Flask
-from app.routes.student_routes import bp as bp_example
+from app.routes.student_routes import bp as bp_student
 from app.routes.employees_routes import bp as bp_employees
+from app.routes.absences_routes import bp as bp_absences
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 def init_app(app: Flask):
-    bp.register_blueprint(bp_example)
+    bp.register_blueprint(bp_student)
     bp.register_blueprint(bp_employees)
+    bp.register_blueprint(bp_absences)
     app.register_blueprint(bp)
