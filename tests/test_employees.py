a825fdf2559e3c184b6354f4d22b0a19e09c31ef
Renato Suguiy <renatosuguiy@gmail.com>
2022-03-06 00:32:05 -0300

    feat(employee_delete): Created tests for the route

--- tests/test_employees.py
+++ tests/test_employees.py
@@ -26,3 +26,25 @@ def test_get_all_employees(client):
     request_response = client.get("/api/employees")
 
     assert (request_response.status_code == 200), "Verificar se o status code é ok"
+
+def test_delete_success(client):
+    request_data = {
+        "name": "Jhon Doe5",
+        "email": "jhondoe5@mail.com",
+        "wage": 3020.90,
+        "access_level": "admin",
+        "password": "1234"
+    }
+
+    request_response = client.post("/api/employees", json=request_data, follow_redirects=True )
+    response_json: dict = request_response.get_json()
+
+    request_delete = client.delete(f"/api/employees/{response_json['employee_id']}")
+
+    assert (request_delete.status_code == 204), "Verificar se o status code é No Content"
+
+def test_delete_error(client):
+
+    request_delete = client.delete(f"/api/employees/b3298cfc-7fb8-47af-91ed-f2d8c4545cdd")
+
+    assert (request_delete.status_code == 404), "Verificar se o status code é Not Found"
