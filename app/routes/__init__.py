b14273fd6f17190188c63535c6ac23f7562a5e1a
Arthur Sarmento <arthurbs17@gmail.com>
2022-03-06 11:14:08 -0300

    feat(get_grades_by_student_id): created function to get grades by student id done

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, Flask
-from app.routes.student_routes import bp as bp_example
+from app.routes.student_routes import bp as bp_students
 from app.routes.employees_routes import bp as bp_employees
+from app.routes.grades_routes import bp as bp_grades
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 def init_app(app: Flask):
-    bp.register_blueprint(bp_example)
+    bp.register_blueprint(bp_students)
     bp.register_blueprint(bp_employees)
+    bp.register_blueprint(bp_grades)
     app.register_blueprint(bp)
