c8f68f5cea6f7fd62813be0c22ee187ac4f2549c
Felipe Silveira <felipelarson@msn.com>
2022-03-07 13:26:50 +0000

    feat(route_library): created route and registered.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, Flask
 from app.routes.students_routes import bp as bp_students
 from app.routes.employees_routes import bp as bp_employees
+from app.routes.library_routes import bp as bp_library
 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -8,4 +9,5 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 def init_app(app: Flask):
     bp.register_blueprint(bp_students)
     bp.register_blueprint(bp_employees)
+    bp.register_blueprint(bp_library)
     app.register_blueprint(bp)
