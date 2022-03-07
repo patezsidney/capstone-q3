0c07cf48effa73ad0cdaf08b58b9eccb39f55179
Felipe Silveira <felipelarson@msn.com>
2022-03-07 05:03:19 +0000

    feat(delete)absence): created controller

--- app/controllers/absense_controllers.py
+++ app/controllers/absense_controllers.py
@@ -1,11 +1,22 @@
+from http import HTTPStatus
+from app.configs.database import db
+from app.models.absence_model import AbsenceModel
+
 def create_absense():
     pass
 
 def update_absense(id: str):
     pass
 
-def delete_absense(id: str):
-    pass
+def delete_absense(absence_id: str):
+    absence: AbsenceModel = AbsenceModel.query.get(absence_id)
+    if absence is None:
+        return {'msg': 'Absence not found'}, HTTPStatus.NOT_FOUND
+        
+    db.session.delete(absence)
+    db.session.commit()
+
+    return {}, HTTPStatus.NO_CONTENT
 
 def get_all_absense():
     pass
