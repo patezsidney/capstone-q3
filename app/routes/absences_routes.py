7d166f65b35fca25be55097a9692c360bb0fbec8
Arthur Sarmento <arthurbs17@gmail.com>
2022-03-07 19:34:38 -0300

    feat(get_all_absences): created function done

--- app/routes/absences_routes.py
+++ app/routes/absences_routes.py
@@ -5,5 +5,6 @@ from app.controllers import absense_controllers
 bp = Blueprint('absences', __name__, url_prefix='/absences')
 
 bp.get("/<student_id>")(absense_controllers.get_student_absense)
+bp.get("")(absense_controllers.get_all_absense)
 bp.patch("/<absence_id>")(absense_controllers.update_absense)
 bp.delete("/<absence_id>")(absense_controllers.delete_absense)
