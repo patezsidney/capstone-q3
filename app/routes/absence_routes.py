88221950bc849a9672eca74f6f739ec545cc2e5c
Sidney Patez <patezsidney.jr@gmail.com>
2022-03-07 11:02:08 -0300

    fix: duplicate absences routes

--- app/routes/absence_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from app.controllers import absense_controllers
-
-bp = Blueprint("absence", __name__, url_prefix="/absence")
-
-bp.delete("/<absence_id>")(absense_controllers.delete_absense)
--- app/routes/absences_routes.py
+++ app/routes/absences_routes.py
@@ -5,3 +5,4 @@ from app.controllers import absense_controllers
 bp = Blueprint('absences', __name__, url_prefix='/absences')
 
 bp.get("/<student_id>")(absense_controllers.get_student_absense)
+bp.delete("/<absence_id>")(absense_controllers.delete_absense)
