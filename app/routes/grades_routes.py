862e215067ea1d96aa4cdab20d3b8f5a50d23cfc
Felipe Silveira <felipelarson@msn.com>
2022-03-07 19:55:27 +0000

    feat(grade_routes): created routes delete

--- app/routes/grades_routes.py
+++ app/routes/grades_routes.py
@@ -6,3 +6,4 @@ bp = Blueprint('grades', __name__, url_prefix='/grades')
 
 bp.get("")(grades_controllers.get_all_grades)
 bp.get("/<student_id>")(grades_controllers.get_student_grades)
+bp.delete("/<grade_id>")(grades_controllers.delete_grade)
