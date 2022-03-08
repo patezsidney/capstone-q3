3f8e49b95f7b91610d6625f9ebabc6d2111ff88a
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2022-03-08 14:35:02 -0300

    feat: add filter get all

--- app/controllers/grades_controllers.py
+++ app/controllers/grades_controllers.py
@@ -1,8 +1,7 @@
 from http import HTTPStatus
-
 from flask import jsonify, request
-from sqlalchemy.exc import DataError
-
+from sqlalchemy.exc import DataError, StatementError
+from werkzeug.exceptions import NotFound
 from app.configs.database import db
 from app.models import GradesModel, StudentsModel
 
@@ -27,9 +26,23 @@ def delete_grade(grade_id: str):
     grade: GradesModel = GradesModel.query.filter_by(grade_id=grade_id).one()
     return jsonify(grade), HTTPStatus.NO_CONTENT
 
+
 def get_all_grades():
-    grades: GradesModel = db.session.query(GradesModel).all()
-    return jsonify(grades), HTTPStatus.OK
+    page = request.args.get("page", 1, type=int)
+    per_page = request.args.get("per_page", 20, type=int)
+    classroom = request.args.get("classroom", type=str)
+    try:
+        if classroom is None:
+            grades: GradesModel = GradesModel.query.paginate(page,per_page)
+            return jsonify(grades.items), HTTPStatus.OK
+        grades: GradesModel = GradesModel.query.filter_by(classrom_id=classroom).paginate(page,per_page)
+        return jsonify(grades.items), HTTPStatus.OK
+    except StatementError:
+        return {"error": "classroom id is invalid"}, HTTPStatus.BAD_REQUEST
+    except NotFound:
+        return {"error": "page not found"}, HTTPStatus.NOT_FOUND
+
+    
 
 # @auth.login_required
 def get_student_grades(student_id: str):
