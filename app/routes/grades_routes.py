a089bac5ccaf5c36534d913c9aa83f1537f7723c
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2022-03-08 10:57:52 -0300

    merge: development to feature/patch_grade

--- app/routes/grades_routes.py
+++ app/routes/grades_routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
-
 from app.controllers import grades_controllers
 
 bp = Blueprint('grades', __name__, url_prefix='/grades')
@@ -8,3 +7,4 @@ bp.get("")(grades_controllers.get_all_grades)
 bp.post("")(grades_controllers.create_grade)
 bp.get("/<student_id>")(grades_controllers.get_student_grades)
 bp.delete("/<grade_id>")(grades_controllers.delete_grade)
+bp.patch("/<id>")(grades_controllers.update_grade)
