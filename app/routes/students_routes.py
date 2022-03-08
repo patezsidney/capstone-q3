cabbbf755a08cd0f0e464a44e386c8e1f3b67e8c
Sidney Patez <patezsidney.jr@gmail.com>
2022-03-08 08:54:47 -0300

    fix: merge conflicts with development

--- app/routes/students_routes.py
+++ app/routes/students_routes.py
@@ -7,12 +7,8 @@ bp = Blueprint('students', __name__, url_prefix='/students')
 bp.post('/login')(students_controllers.signin)
 bp.get('')(students_controllers.get_all_students)
 bp.get('/profile')(students_controllers.get_student_by_api_key)
-<<<<<<< HEAD
-bp.get('/<student_id>')(students_controllers.get_student_by_id)
-=======
 
 bp.get('/<student_id>')(students_controllers.get_student_by_id)
 
 bp.patch("/<student_id>")(students_controllers.update_student)
 
->>>>>>> development
