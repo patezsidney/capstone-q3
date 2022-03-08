5ebe2bcc5c2cfd869b684dd98570b96711e84026
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2022-03-08 16:08:35 -0300

    feat: alteration decorator verify_some_key

--- app/services/decorators.py
+++ app/services/decorators.py
@@ -7,12 +7,11 @@ from flask import request
 def verify_some_keys(trusted_keys: "list[str]"):
     def test_key(func):
         @wraps(func)
-        def wraped_function(employee_id):
+        def wraped_function(employee_id=None):
             try:
                 data = request.get_json()
                 for key in data.keys():
                     if key not in trusted_keys:
-                        print(key)
                         raise KeyError
                 return func(employee_id)
             except KeyError:
