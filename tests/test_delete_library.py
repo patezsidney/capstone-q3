5a1ab882d626f94eb3ff1d53ecce35514eac5b15
Renato Suguiy <renatosuguiy@gmail.com>
2022-03-08 23:17:45 -0300

    feat(tests): Correct library tests to consider the delete test

--- tests/test_delete_library.py
+++ tests/test_delete_library.py
@@ -1,6 +1,6 @@
 from flask.testing import FlaskClient
 
 def test_delete_library(client: FlaskClient):
-    request_delete = client.delete("/api/library/3554e9f0-8208-4e99-81c1-d79f3caf891c")
+    request_delete = client.delete("/api/library/023b926d-8f03-460d-be7b-840d80f91f6e")
 
     assert (request_delete.status_code == 204), "Verificar se o status code é No Content"
