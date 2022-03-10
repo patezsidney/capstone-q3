b09d9e11409984a66d3fef8b1836d8f8fa85f74c
rafaelgsousa <elderrafaelgomes@gmail.com>
2022-03-09 23:19:58 -0300

    feature(school_subjects_controllers):register_teacher_in_school_subject function creation

---
+++ app/controllers/school_subjects_controllers.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from http import HTTPStatus
+
+from flask import current_app, jsonify, request
+from sqlalchemy import null
+from sqlalchemy.exc import DataError
+from sqlalchemy.orm.session import Session
+from werkzeug.exceptions import NotFound
+
+from app.configs.auth import auth_employee
+from app.configs.database import db
+from app.models.exc import IncorrectKeyError, MissingKeyError, TypeValueError
+from app.models.school_subjects_model import SchoolSubjectsModel
+
+def register_teacher_in_school_subject():
+    data = request.get_json()
+
+    school_subject = SchoolSubjectsModel(**data)
+
+    current_app.db.session.add(school_subject)
+    current_app.db.session.commit()
+
+    return {
+            "school_subject_id":school_subject.school_subject_id,
+            "school_subject":school_subject.school_subject,
+            "classroom":school_subject.classroom.name,
+            "teacher":school_subject.employee.name
+            },HTTPStatus.CREATED
